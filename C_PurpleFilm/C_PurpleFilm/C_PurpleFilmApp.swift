641d1737c067140a746f848d0e350e74d07185e1
Hayun218 <gkahsdl@icloud.com>
2023-09-11 17:12:29 +0900

    [INIT] init project

---
+++ C_PurpleFilm/C_PurpleFilm/C_PurpleFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  C_PurpleFilmApp.swift
+//  C_PurpleFilm
+//
+//  Created by yun on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct C_PurpleFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
